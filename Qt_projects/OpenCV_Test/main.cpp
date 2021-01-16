152f4d66e7f441499bc238de4ef66000c577687c
HidingSky <823021875@qq.com>
2021-01-16 18:29:13 +0800

    new

---
+++ Qt_projects/OpenCV_Test/main.cpp
@@ -0,0 +1,15 @@
+#include "widget.h"
+#include <iostream>
+#include <QApplication>
+//#include "opencv.hpp"
+//#include "opencv_modules.hpp"
+//using namespace cv;
+int main(int argc, char *argv[])
+{
+
+//    VideoCapture capture(0);
+    QApplication a(argc, argv);
+    Widget w;
+    w.show();
+    return a.exec();
+}
